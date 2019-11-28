4302bac825ddfba96576f146f5eb188a5666768c
Jonny Edwards <jonathan44472@gmail.com>
2019-11-28 12:59:06 +0000

    Random color button now works

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const input = () => {
-  let query = document.querySelector("input").value;
+const handleCircle = query => {
   let body = document.body;
   let circle = document.getElementById("dot");
-
-  if (!query.startsWith("#")) {
-    query = `#${query}`;
-  }
 
   // Manages circle
   setTimeout(() => {
@@ -27,9 +22,28 @@ const input = () => {
   circle.style.width = "4096px";
 };
 
+const input = () => {
+  let query = document.querySelector("input").value;
+
+  if (!query.startsWith("#")) {
+    query = `#${query}`;
+  }
+
+  handleCircle(query);
+};
+
 const inputShort = e => {
   // Checks if enter key is pressed
   e.keyCode == 13 ? input() : null;
 };
 
 // When creating the random color function, create a new function taking all the circle deletion/creation etc. to save writing is again
+const randomColor = () => {
+  const newHex = () => {
+    return `#${Math.random()
+      .toString(16)
+      .slice(2, 8)}`;
+  };
+
+  handleCircle(newHex());
+};
